ca9f5e4bf3ae42a61c85672029d4adc05c89fcf6
Apoorv-cloud <63242652+Apoorv-cloud@users.noreply.github.com>
2021-07-11 21:25:23 +0530

    player

--- lib/data/repository/AudioPlayerModelFactory.dart
+++ lib/data/repository/AudioPlayerModelFactory.dart
@@ -18,79 +18,91 @@ class AudioPlayerModelFactory {
       AudioPlayerModel(
           id: "2",
           isPlaying: false,
-          audio: Audio("assets/audios/meditation_piano.mp3",
+          audio: Audio.network(
+              "https://drive.google.com/uc?export=view&id=1998EymDn7Yup0sun2Fm0Ed6GdEhQCgrS",
               metas: Metas(
                 id: "2",
-                title: "Soothing Piano",
-                artist: "Joe Doe",
+                title: "Creative Minds",
+                artist: "Venino",
                 album: "Country Album",
-                image: MetasImage.asset("assets/piano.jpg"),
+                image: MetasImage.network(
+                    "https://www.bensound.com/bensound-img/creativeminds.jpg"),
               ))),
       AudioPlayerModel(
           id: "3",
           isPlaying: false,
-          audio: Audio("assets/audios/meditation_stones.mp3",
+          audio: Audio.network(
+              "https://drive.google.com/uc?export=view&id=1G2MPHYspQlhDWt_vXasy_ufHWO2rGCuV",
               metas: Metas(
                 id: "3",
-                title: "Meditation Stones",
-                artist: "Joe Doe",
-                album: "Country Album",
-                image: MetasImage.asset("assets/st.jpg"),
+                title: "Ukulele",
+                artist: "Benjamin Tissot",
+                album: "Feel Good",
+                image: MetasImage.network(
+                    'https://www.bensound.com/bensound-img/ukulele.jpg'),
               ))),
       AudioPlayerModel(
           id: "4",
           isPlaying: false,
-          audio: Audio("assets/audios/meditation_wind_chimes.mp3",
+          audio: Audio.network(
+              "https://drive.google.com/uc?export=view&id=1oF07Gr-h1Hr3Ef1mhWK9Txt4fAmZZ0RL",
               metas: Metas(
                 id: "4",
-                title: "Wind Chimes",
-                artist: "Joe Doe",
+                title: "A New Beginning",
+                artist: "Olga Zakharova",
                 album: "Country Album",
-                image: MetasImage.asset("assets/wind.jpg"),
+                image: MetasImage.network(
+                    "https://www.bensound.com/bensound-img/anewbeginning.jpg"),
               ))),
       AudioPlayerModel(
           id: "5",
           isPlaying: false,
-          audio: Audio("assets/audios/country_2.mp3",
+          audio: Audio.network(
+              "https://drive.google.com/uc?export=view&id=10CNGHvzdVMl9whB7txD9kEoBHItkk--d",
               metas: Metas(
                 id: "5",
-                title: "My Other Country Song",
-                artist: "Joe Doe",
+                title: "Little Idea",
+                artist: "Dooder",
                 album: "Country Album",
-                image: MetasImage.asset("assets/country.jpg"),
+                image: MetasImage.network(
+                    "https://www.bensound.com/bensound-img/littleidea.jpg"),
               ))),
       AudioPlayerModel(
           id: "6",
           isPlaying: false,
-          audio: Audio("assets/audios/country.mp3",
+          audio: Audio.network(
+              "https://drive.google.com/uc?export=view&id=14MrxKE6zaK6py-riEnhsNTogX5v0czZ9",
               metas: Metas(
                 id: "6",
-                title: "My Country Song",
-                artist: "Joe Doe",
+                title: "Memories",
+                artist: "Galyna Andrushko",
                 album: "Country Album",
-                image: MetasImage.asset("assets/country.jpg"),
+                image: MetasImage.network(
+                    "https://www.bensound.com/bensound-img/memories.jpg"),
               ))),
       AudioPlayerModel(
           id: "7",
           isPlaying: false,
-          audio: Audio("assets/audios/country_2.mp3",
+          audio: Audio.network(
+              "https://drive.google.com/uc?export=view&id=1YZChlIrJolCrcUutjaWafCZHr02FVTIA",
               metas: Metas(
                 id: "7",
-                title: "My Other Country Song",
-                artist: "Joe Doe",
+                title: "Going Higher",
+                artist: "Radoman Durkovic",
                 album: "Country Album",
-                image: MetasImage.asset("assets/country.jpg"),
+                image: MetasImage.network(
+                    "https://www.bensound.com/bensound-img/goinghigher.jpg"),
               ))),
       AudioPlayerModel(
           id: "8",
           isPlaying: false,
-          audio: Audio("assets/audios/country_2.mp3",
+          audio: Audio("",
               metas: Metas(
                 id: "8",
                 title: "My Other Country Song",
                 artist: "Joe Doe",
                 album: "Country Album",
-                image: MetasImage.asset("assets/country.jpg"),
+                image: MetasImage.asset(""),
               ))),
     ];
   }
